b02db53517722c0c6313bf7f34963310a7b9fea9
Yeongdong <jyd37855@gmail.com>
2024-02-22 15:33:12 +0900

    스프링 로드맵 1 - 스프링 핵심 원리 학습 프로젝트

--- src/main/java/hello/hellospring/repository/JdbcTemplateMemberRepository.java
+++ src/main/java/hello/hellospring/repository/JdbcTemplateMemberRepository.java
@@ -1,7 +1,6 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
-import hello.hellospring.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
