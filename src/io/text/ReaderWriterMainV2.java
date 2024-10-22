296e8bc11f367a1521bc251a166a6e0535738d9f
Yeongdong <jyd37855@gmail.com>
2024-10-22 15:39:44 +0900

    I/O basics2 - text I/O

---
+++ src/io/text/ReaderWriterMainV2.java
@@ -0,0 +1,33 @@
+package io.text;
+
+import java.io.*;
+import java.util.Arrays;
+
+import static io.text.TextConst.FILE_NAME;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class ReaderWriterMainV2 {
+    public static void main(String[] args) throws IOException {
+        String writeString = "ABC";
+        System.out.println("write String: " + writeString);
+
+        // 파일에 쓰기
+        FileOutputStream fos = new FileOutputStream(FILE_NAME);
+        OutputStreamWriter osw = new OutputStreamWriter(fos, UTF_8);    // 문자를 입력받고, 받은 문자를 인코딩해서 byte[]로 변환
+        osw.write(writeString);
+        osw.close();
+
+        // 파일에서 읽기
+        FileInputStream fis = new FileInputStream(FILE_NAME);
+        InputStreamReader isr = new InputStreamReader(fis, UTF_8);      // 읽은 byte[]을 문자인 char로 변경해서 반환
+
+        StringBuilder content = new StringBuilder();
+        int ch;
+        while ((ch = isr.read()) != -1) {
+            content.append((char) ch);
+        }
+        isr.close();
+
+        System.out.println("read String: " + content);
+    }
+}
