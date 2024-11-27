9b9a8af4c5e6761b9928e7e87758f90cc02df46a
Yeongdong <jyd37855@gmail.com>
2024-11-27 15:55:52 +0900

    http server - Member management program

--- src/was/httpserver/HttpRequest.java
+++ src/was/httpserver/HttpRequest.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static java.nio.charset.StandardCharsets.*;
+import static util.MyLogger.log;
 
 public class HttpRequest {
     private String method;
@@ -17,7 +18,7 @@ public class HttpRequest {
     public HttpRequest(BufferedReader reader) throws IOException {
         parseRequestLine(reader);
         parseHeaders(reader);
-        // 메시지 바디는 이후에 처리
+        parseBody(reader);
     }
 
     private void parseRequestLine(BufferedReader reader) throws IOException {
@@ -54,6 +55,25 @@ public class HttpRequest {
             String key = URLDecoder.decode(keyValue[0], UTF_8);
             String value = keyValue.length > 1 ? URLDecoder.decode(keyValue[1], UTF_8) : "";
             queryParameters.put(key, value);
+        }
+    }
+
+    private void parseBody(BufferedReader reader) throws IOException {
+        if (!headers.containsKey("Content-Length")) {
+            return;
+        }
+        int contentLength = Integer.parseInt(headers.get("Content-Length"));
+        char[] bodyChars = new char[contentLength];
+        int read = reader.read(bodyChars);
+        if (read != contentLength) {
+            throw new IOException("Failed to read entire body. Expected " +
+                    contentLength + " bytes, but read " + read);
+        }
+        String body = new String(bodyChars);
+        log("HTTP Message Body: " + body);
+        String contentType = headers.get("Content-Type");
+        if ("application/x-www-form-urlencoded".equals(contentType)) {
+            parseQueryParameters(body);
         }
     }
 
